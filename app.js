18126d01c9b71d24d9c842726bfbe6434762562a
Karan Jain <36040615+Karan-Jain-NY@users.noreply.github.com>
2021-10-21 01:22:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+//Modules config
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const moment = require("moment");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const methodOverride = require("method-override");
+
+//Mongoose models config
+const User = require("./models/User");
+const Property = require("./models/Property");
+const Comment = require("./models/Comment");
+
+//Routes config
+const indexAuthRoute = require("./routes/index");
+const usersRoutes = require("./routes/user");
+const usersReviewsRoutes = require("./routes/review");
+const propertiesBuyRoutes = require("./routes/properties/buy");
+const propertiesRentRoutes = require("./routes/properties/rent");
+const commentsBuyRoutes = require("./routes/comments/buy");
+const commentsRentRoutes = require("./routes/comments/rent");
+const searchBuy = require("./routes/searchFilter/buy");
+const searchRent = require("./routes/searchFilter/rent");
+
+//Application config
+let url = "add your own string";
+mongoose.connect(url, {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+//Authentication config
+app.use(require("express-session")({
+    secret: "So many books, so little time",
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//Local Midleware config
+app.use((req, res, next)=>{
+    res.locals.user = req.user;
+    res.locals.error = req.flash("error");
+    res.locals.success = req.flash("success");
+    res.locals.moment = moment;
+    res.locals.timeStamp = Date.now();
+    next();
+});
+
+//Serving usage configuration
+app.use("/", indexAuthRoute);
+app.use("/real-estates/users", usersRoutes);
+app.use("/real-estates/users/:id/reviews", usersReviewsRoutes);
+app.use("/real-estates/buy", propertiesBuyRoutes);
+app.use("/real-estates/rent", propertiesRentRoutes);
+app.use("/real-estates/buy/:id/comments", commentsBuyRoutes);
+app.use("/real-estates/rent/:id/comments", commentsRentRoutes);
+app.use("/real-estates/search/buy", searchBuy);
+app.use("/real-estates/search/rent", searchRent);
+
+
+//Error 404 route
+app.get("*", (req, res)=> res.send("Page not found"));
+
+
+//Express listening route
+let port = process.env.PORT;
+if(port==null || port==""){
+  port = 3000;
+}
+app.listen(port, ()=> console.log("Application has started at port 3000!"));
